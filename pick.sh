23969ccd3a030b68865e5634d83941d2f5f4d411
0-kaladin <0.kaladin@gmail.com>
2013-05-11 20:31:23 -0400

    Disable pick.sh, just left for reference

--- pick.sh
+++ pick.sh
@@ -1,13 +1,15 @@
+#### YOU SHOULDN'T NEED TO USE THIS  #####
+#### LOOK AT THE README IN PLATFORM_MANIFEST ####
 #run me for frameworks picks
 
 # cd frameworks/base
 # git fetch http://gerrit.aokp.co/AOKP/frameworks_base refs/changes/33/4633/2 && git cherry-pick FETCH_HEAD
 
-cd frameworks/av
-git fetch http://gerrit.aokp.co/AOKP/frameworks_av refs/changes/34/4634/1 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.aokp.co/AOKP/frameworks_av refs/changes/15/4615/2 && git cherry-pick FETCH_HEAD
+# cd frameworks/av
+# git fetch http://gerrit.aokp.co/AOKP/frameworks_av refs/changes/34/4634/1 && git cherry-pick FETCH_HEAD
+# git fetch http://gerrit.aokp.co/AOKP/frameworks_av refs/changes/15/4615/2 && git cherry-pick FETCH_HEAD
 
 # cd ../native
 # git fetch http://gerrit.aokp.co/AOKP/frameworks_native refs/changes/35/4635/1 && git cherry-pick FETCH_HEAD
 
-cd ../../
+# cd ../../
